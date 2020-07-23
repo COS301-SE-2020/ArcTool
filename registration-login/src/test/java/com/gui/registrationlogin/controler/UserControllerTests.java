76b35fa4d88c1427be1e221be93f89e5c018132e
Mxolisi-Mkanzi <u18059326@tuks.co.za>
2020-07-23 15:26:50 -0700

    Test for UserController created
    A testing class for userController created

--- registration-login/src/test/java/com/gui/registrationlogin/controler/UserControllerTests.java
+++ registration-login/src/test/java/com/gui/registrationlogin/controler/UserControllerTests.java
@@ -1,10 +1,31 @@
 package com.gui.registrationlogin.controler;
 
+import com.gui.registrationlogin.controller.UserController;
+import com.gui.registrationlogin.model.Path;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.BindingResult;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UserControllerTests {
+
+    @Test
+    void extract(){
+        UserController userController = new UserController();
+        Path path = new Path();
+        String p= "";
+        path.setFR("Bake");
+        path.setPath(p);
+        try{
+            //Set p to a correct path for this test to pass and set it to a wrong path for it to fail
+            assertEquals("File created", userController.extract(path, null));
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+
+    }
 }

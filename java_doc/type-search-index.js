6044a2c868cb178c299a2c59729c47680906019e
WandaWeira <55826359+WandaWeira@users.noreply.github.com>
2020-08-06 18:14:08 +0200

    Java Documentation
    This shows the JavaDocs for different classes.

---
+++ java_doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Distance"},{"p":"<Unnamed>","l":"Edge"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Node"},{"p":"<Unnamed>","l":"Trie"}]

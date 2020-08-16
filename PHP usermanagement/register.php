3471f178e2ec75d48619ebdcbb2ec7da35d43d12
HappinessMasombuka <u16324804@tuks.co.za>
2020-08-16 14:58:29 +0200

    Added bootstrap to user management pages to match theme of UI

--- PHP usermanagement/register.php
+++ PHP usermanagement/register.php
@@ -2,8 +2,10 @@
 <!DOCTYPE>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <meta http-equiv=”Content-Type” content=”text/html; charset=utf-8″ />
-<title>User Management System </title>
-<link rel="stylesheet" href="style.css" type="text/css" />
+<title>Register</title>
+<!-- <link rel="stylesheet" href="style.css" type="text/css" /> -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div id="main">
@@ -41,16 +43,101 @@ else
 {
 ?>
 
+<div class="container-fluid bg-light text-center pt-3 pb-3"  style="margin-bottom:0">
+        <h1>ArcTool</h1> 
+      </div>
+      
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" th:href="@{/index}">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" th:href="@{/signup}">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" th:href="@{/login}">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" th:href="@{/index}">Logout</a>
+            </li>    
+          </ul>
+        </div>  
+      </nav>
+
+<div class="container text-center pt-5 pb-5">
 <h1>Register</h1>
 
 <p>Please enter your details below to register.</p>
 
-<form method="post" action="formcheck.php" name="registerform" id="registerform">
+<form class="form-horizontal" method="post" action="formcheck.php" name="registerform" id="registerform">
 <fieldset>
-<label for="username">Username:</label><input type="text" name="username" id="username" /><br />
-<label for="password">Password:</label><input type="password" name="password" id="password" /><br />
-<label for="email">Email Address:</label><input type="text" name="email" id="email" /><br />
-<input type="submit" name="register" id="register" value="Register" />
+<!-- <label for="username">Username:</label><input type="text" name="username" id="username" /><br /> -->
+<div class="row">
+    <div class="col-md-3 field-label-responsive">
+        <label for="username">Username:</label>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+                    <input type="text" name="username" class="form-control" id="username"
+                        placeholder="Username" required autofocus>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- <label for="password">Password:</label><input type="password" name="password" id="password" /><br /> -->
+<div class="row">
+    <div class="col-md-3 field-label-responsive">
+        <label for="email">Email address:</label>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-at"></i></div>
+                    <input type="text" name="email" class="form-control" id="email"
+                        placeholder="username@something.com" required autofocus>
+                </div>
+            </div>
+        </div>
+             <!-- <div class="col-md-3">
+                 <div class="form-control-feedback">
+                         <span class="text-danger align-middle" th:if="${#fields.hasErrors('email')}" th:errors="*{email}">
+                         </span>
+                 </div>
+             </div> -->
+</div>
+
+<!-- <label for="email">Email Address:</label><input type="text" name="email" id="email" /><br /> -->
+
+<div class="row">
+    <div class="col-md-3 field-label-responsive">
+        <label for="password">Password:</label>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group has-danger">
+            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-key"></i></div>
+                <input type="password" name="password" class="form-control" id="password"
+                    placeholder="Password" required>
+                </div>
+            </div>
+        </div>
+             <!-- <div class="col-md-3">
+                 <div class="form-control-feedback">
+                         <span class="text-danger align-middle" th:if="${#fields.hasErrors('password')}" th:errors="*{password}">
+                         </span>
+                 </div>
+             </div> -->
+</div>
+<input type="submit" color="gray" name="register" id="register" value="Register" />
 </fieldset>
 </form>
 
